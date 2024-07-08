ace5dc9655a1491609ad49a7bb82b9c16e2683d2
Marcus Vinicius Carneiro da Silva <marcusvinicius19112004@gmail.com>
2024-07-08 11:27:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(frase) {
+  let reverse = " ";
+  let lowerCase = frase.toLowerCase().replaceAll(" ", "");
+
+  for (let i = lowerCase.length; i > 0; i--) {
+    reverse = reverse += lowerCase[i];
+    
+    return true;
+  }
+  return false;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+  let maxnum = arr[0];
+  let minnum = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < minnum) {
+      minnum = arr[i];
+    }
+    if (arr[i] > maxnum) {
+      maxnum = arr[i];
+    }
+  }
+  return [minnum, maxnum];
 }
